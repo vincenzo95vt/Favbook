9df1b4c7a7942738d2d72a1356f1d472b75f4ff9
Vinnie <pablovasta1995@gmail.com>
2024-04-30 01:25:13 +0200

    conexion back con front, no terminado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './styles.scss'; 
-import  {} "./js/DOM/create-dom"
+import  "./js/DOM/create-dom"
+import {connectionApi} from "./js/api/apiConnectionBack"
+
+connectionApi()
---
+++ src/js/api/apiConnectionBack.js
@@ -0,0 +1,97 @@
+
+export function connectionApi(){
+
+    const loginElem = document.getElementById("controllers")
+    loginElem.addEventListener("submit", async function(event) {
+        console.log("clicked")
+        event.preventDefault()
+        
+        const loginData = new FormData(this)
+        const email = loginData.get("email")
+        console.log(email)
+        const password = loginData.get("password")
+        console.log(password)
+        
+
+
+        try {
+            const response = await fetch("http://localhost:4000/user/login", {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({email, password}),
+            });
+            const data = await response.json();
+            //Guardamos el token  en localStorage para futuras peticiones
+            if(data) {
+                localStorage.setItem('token', data.token);
+                localStorage.setItem("token_refresh", data.token_refresh);
+            };
+            await fetchPosts()
+        } catch (error) {
+            console.error("Error: Cannot get the data")
+    
+        }
+        async function fetchPosts(){
+            try {
+                const token = localStorage.getItem("token")
+                if(!token){
+                    throw new Error("Token not found");
+                }
+                const response = await fetch("http://localhost:4000/posts", {
+                    headers: {
+                        "auth-token": token
+                    },
+                });
+    
+                if(response.status === 400) {
+                    //Token caducado, llamar a refreshToken en caso de no haberse ejecutado. 
+    
+                    await refreshToken();
+    
+                }else{
+                    throw new Error(`HTTP error! status: ${response.status}`)
+                }
+    
+                const data = await response.json()
+                console.log(data) //Vamos a ver que nos devuelve para manejar los datos a nuestra manera.
+    
+                const posts = data.data;
+                return posts
+            } catch (error) {
+                console.error("Error fetching posts:", error);
+            }
+        }
+    
+        async function refreshToken(){
+            try {
+                const token_refresh = localStorage.getItem("token_refresh");
+                console.log(token_refresh);
+    
+                const response = await fetch("http://localhost:4000/user/refreshToken", {
+                    method:"POST",
+                    headers:{
+                        "auth-token": token_refresh,
+                    }
+                }
+            );
+    
+            const newData = await response.json();
+    
+            if(newData) {
+                localStorage.setItem('token', newData.token);
+                localStorage.setItem("token_refresh", newData.token_refresh);
+            }
+            await fetchPosts()
+            } catch (error) {
+                console.error("Error refreshing token:", error)
+            }
+        }
+        window.onload = async () => {
+            await refreshToken()
+        }
+    })
+}
+
+//PROBLEMAS PARA CONECTAR BACK CON FRONT.
